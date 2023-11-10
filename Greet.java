d4161b06f848576cd29809e25513782eb9ad5112
Thomas McLaughlin <tmclaughlin5.1111@gmail.com>
2023-11-10 12:04:56 +0000

    added conflict

--- Greet.java
+++ Greet.java
@@ -1,6 +1,7 @@
 public class Greet {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("How are you?");  // ADDED
+        String salutation = "Good morning";  // MODIFIED
+        System.out.println(salutation);  // MODIFIED
+        System.out.println("How are you?"); 
     }
 }
